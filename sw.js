fc12b7e6d0c200be8aa2d4251a6df82bf96c15fa
TomoyaKiriyama <kiriyama.github@gmail.com>
2026-09-13 10:16:21 +0900

    Keep first visit greeting on one line on small screens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='medaka-v2-2';
+const CACHE='medaka-v2-3';
 const FILES=['./','./index.html','./style.css','./app.js','./core.js','./aquarium.js','./assets/medaka.png','./assets/icon.svg','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('medaka-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
